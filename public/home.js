ca41d0f3a80d8c5f334d1adcfa3980e5aa3861a2
Didi <didi.zlatkova93@gmail.com>
2015-12-19 19:03:18 +0200

    Implemented calculation of time for the setup program

--- public/home.js
+++ public/home.js
@@ -1,43 +1,79 @@
-$('.my-checkbox').bootstrapSwitch();
+(function() {
 
-$('.my-slider').slider({
-    formatter: function(value) {
-        return 'Времетраене в минути: ' + value;
-    }
-});
+    $('.my-checkbox').bootstrapSwitch();
 
-$("#prewash-slider").on("slide", function(slideEvt) {
-    $("#prewash-slider-value").text(slideEvt.value);
-});
+    $('.my-slider').slider({
+        formatter: function(value) {
+            return 'Времетраене в минути: ' + value;
+        }
+    });
 
-$("#wash-slider").on("slide", function(slideEvt) {
-    $("#wash-slider-value").text(slideEvt.value);
-});
+    $("#prewash-slider").on("slide", function(slideEvt) {
+        $("#prewash-slider-value").text(slideEvt.value);
+    });
 
-$("#rinse-slider").on("slide", function(slideEvt) {
-    $("#rinse-slider-value").text(slideEvt.value);
-});
+    $("#wash-slider").on("slide", function(slideEvt) {
+        $("#wash-slider-value").text(slideEvt.value);
+    });
 
-$('input[name="prewash"]').on('switchChange.bootstrapSwitch', function(event, state) {
-    if (state) {
-        $("#prewash-slider").slider("enable");
-    } else {
-        $("#prewash-slider").slider("disable");
-    }
-});
+    $("#rinse-slider").on("slide", function(slideEvt) {
+        $("#rinse-slider-value").text(slideEvt.value);
+    });
 
-$('input[name="wash"]').on('switchChange.bootstrapSwitch', function(event, state) {
-    if (state) {
-        $("#wash-slider").slider("enable");
-    } else {
-        $("#wash-slider").slider("disable");
-    }
-});
+    $('input[name="prewash"]').on('switchChange.bootstrapSwitch', function(event, state) {
+        if (state) {
+            $("#prewash-slider").slider("enable");
+        } else {
+            $("#prewash-slider").slider("disable");
+        }
+    });
 
-$('input[name="rinse"]').on('switchChange.bootstrapSwitch', function(event, state) {
-    if (state) {
-        $("#rinse-slider").slider("enable");
-    } else {
-        $("#rinse-slider").slider("disable");
-    }
-});
+    $('input[name="wash"]').on('switchChange.bootstrapSwitch', function(event, state) {
+        if (state) {
+            $("#wash-slider").slider("enable");
+        } else {
+            $("#wash-slider").slider("disable");
+        }
+    });
+
+    $('input[name="rinse"]').on('switchChange.bootstrapSwitch', function(event, state) {
+        if (state) {
+            $("#rinse-slider").slider("enable");
+        } else {
+            $("#rinse-slider").slider("disable");
+        }
+    });
+
+    $(":input").change(function() {
+        calculateTotalTime();
+    });
+
+    $('.my-checkbox').on('switchChange.bootstrapSwitch', function(event, state) {
+        calculateTotalTime();
+    });
+
+    var calculateTotalTime = function() {
+        var time = 0;
+        if ($('input[name="prewash"]').is(':checked')) {
+            time += $("#prewash-slider").slider('getValue');
+        }
+
+        if ($('input[name="wash"]').is(':checked')) {
+            time += $("#wash-slider").slider('getValue');
+        }
+
+        if ($('input[name="rinse"]').is(':checked')) {
+            time += $("#rinse-slider").slider('getValue');
+        }
+
+        if ($('input[name="centrifuge"]').is(':checked')) {
+            time += 10;
+        }
+
+        if ($('input[name="iron"]').is(':checked')) {
+            time += 5;
+        }
+
+        $("#time").text(time);
+    };
+})();
